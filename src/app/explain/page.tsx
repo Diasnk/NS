f504e33823e3bb81fcdaa3e1e62bc16916b3c41e
Diasnk <diasnugumanov258@gmail.com>
2023-10-30 15:08:00 +0600

    new

---
+++ src/app/explain/page.tsx
@@ -0,0 +1,12 @@
+import ExplanationComponent from '@/components/explanationComponent'
+import Image from 'next/image'
+
+export default function Explain() {
+	return (
+		<>
+            <div>
+                <ExplanationComponent/>
+            </div>
+		</>
+	)
+}
