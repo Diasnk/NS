430368e60419eccfaa59b3795ebe52f3f1aea012
MugiD <slambek_b0607@gmail.com>
2023-10-30 18:54:34 +0600

    remove div

--- src/app/explain/page.tsx
+++ src/app/explain/page.tsx
@@ -1,12 +1,9 @@
 import Features from "@/components/Features";
-import Image from "next/image";
 
 export default function Explain() {
   return (
     <>
-      <div>
       <Features path="api/exp" text="Explain"/>
-      </div>
     </>
   );
 }
--- src/app/simplify/page.tsx
+++ src/app/simplify/page.tsx
@@ -1,12 +1,9 @@
 import Features from "@/components/Features";
-import Image from "next/image";
 
 export default function Explain() {
   return (
     <>
-      <div>
         <Features path="api/simp" text="Simplify"/>
-      </div>
     </>
   );
 }
